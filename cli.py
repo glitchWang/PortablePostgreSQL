5f7ff6553ed13bff4e5ed96992f000b9d6db3f1c
ew <glitch.wang.au@gmail.com>
2015-03-06 23:35:43 +1100

    command line tool: list environment and init cluster

--- cli.py
+++ cli.py
@@ -1,5 +1,7 @@
 #-*-coding: utf-8-*-
+from __future__ import print_function
 import click
+import pg
 
 
 @click.group()
@@ -9,7 +11,21 @@ def cli():
 
 @click.command()
 def ls():
-    print()
+    engines = pg.Database.db_engines()
+    click.echo('\n'.join([repr(e) for e in engines]))
+
+
+@click.command()
+@click.argument('ver')
+def init(ver):
+    engine_path = './App/{}'.format(ver)
+    engine = pg.Database(engine_path)
+    if not engine.cluster_dir_exists:
+        engine.init_cluster()
+
+
+cli.add_command(ls)
+cli.add_command(init)
 
 if __name__ == '__main__':
     cli()
